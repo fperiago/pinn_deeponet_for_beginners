ba788fca1fb2bfbde99fb830aeb68b0c7d9c0e63
Paco Periago <f.periago@upct.es>
2025-07-02 10:32:25 +0200

    update folders

--- README.md
+++ README.md
@@ -32,10 +32,10 @@ In the following link you will find the data files needed in some examples. ```
 ```
 @book {gar26,
 	AUTHOR = {Garc\'{\i}a-Cervera, C. J. and Kessler, M. and Pedregal, P. and Periago, F.},
-	TITLE = {Physics-Informed Neural Networks and Deep Operator Network for beginners	},
-	SERIES = {Lecture Notes of the XXI Jacques-Louis Lions Spanish-French School, Ciudad Real, 2025},
+	TITLE = {Physics-Informed Neural Networks and Deep Operator Network for beginners},
+	SERIES = {Lecture Notes of the XXI Jacques-Louis Lions Spanish-French School Ciudad Real, 2025},
 	VOLUME = {},
-	PUBLISHER = {SEMA-SIMA Springer},
+	PUBLISHER = {SEMA-SIMA Springer Series},
 	YEAR = {2026},
 	PAGES = {},
 	ISBN = {},
---
+++ pinn_course/src/utilities.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def compute_eig(kernel, num, Nx, eigenfunction=True):
+    """
+    Computes the eigenvalues and ortonormalized eigenfunctions of a kernel on [0, 1].
+    Returns a tuple eigval, eigvec, where eigval contains the num largest eigen values, and
+    eigvec is a matrix of dimension Nx times num. Column j corresponds to
+    eigenfuncion associated to eigenvalue eigval[j], evaluated at np.linspace(0, 1, num=Nx).
+    """
+    # We consider a discretization grid of width 0.01 on [0, 1]
+    num_discretization = np.max([100, Nx]) + 1
+    if num_discretization < num:
+        raise ValueError(
+            """The grid in the kernel discretization to compute the eigenfunctions is not sufficient.
+            There are more than one hundred terms required in the KL expansion"""
+        )
+    h = 1 / (num_discretization - 1)
+    c = kernel(np.linspace(0, 1, num=num_discretization)[:, None])[0] * h
+    A = np.empty((num_discretization, num_discretization))
+    for i in range(num_discretization):
+        A[i, i:] = c[: num_discretization - i]
+        A[i, i::-1] = c[: i + 1]
+    A[:, 0] *= 0.5
+    A[:, -1] *= 0.5
+
+    if not eigenfunction:
+        return np.flipud(np.sort(np.real(np.linalg.eigvals(A))))[:num]
+
+    eigval, eigvec = np.linalg.eig(A)
+    eigval, eigvec = np.real(eigval), np.real(eigvec)
+    idx = np.flipud(np.argsort(eigval))[:num]
+    eigval, eigvec = eigval[idx], eigvec[:, idx]
+    integrals = np.trapz(eigvec ** 2, dx=h, axis=0) ** 0.5
+    eigvec /= integrals.reshape(1, -1)
+    indexes = (np.round(np.linspace(0, 1, num=Nx), 2) * 100).astype(int)
+    return eigval, eigvec[indexes, :]
