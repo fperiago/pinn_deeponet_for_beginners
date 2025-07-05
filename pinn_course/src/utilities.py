24a4db033cd265b8f603b9d7504a3b9c575096ab
Paco Periago <f.periago@upct.es>
2025-07-05 17:41:52 +0200

    update codes and slides

--- pinn_course/src/utilities.py
+++ pinn_course/src/utilities.py
@@ -4,7 +4,7 @@ import numpy as np
 def compute_eig(kernel, num, Nx, eigenfunction=True):
     """
     Computes the eigenvalues and ortonormalized eigenfunctions of a kernel on [0, 1].
-    Returns a tuple eigval, eigvec, where eigval contains the num largest eigen values, and
+    Returns a tuple eigval, eigvec, where eigval contains the num largest eigenvalues, and
     eigvec is a matrix of dimension Nx times num. Column j corresponds to
     eigenfuncion associated to eigenvalue eigval[j], evaluated at np.linspace(0, 1, num=Nx).
     """
@@ -15,23 +15,24 @@ def compute_eig(kernel, num, Nx, eigenfunction=True):
             """The grid in the kernel discretization to compute the eigenfunctions is not sufficient.
             There are more than one hundred terms required in the KL expansion"""
         )
-    h = 1 / (num_discretization - 1)
-    c = kernel(np.linspace(0, 1, num=num_discretization)[:, None])[0] * h
+    h = 1 / (num_discretization - 1) # discretization step 
+    # first row of the kernel times h (trapezoidal integration) Kernel(0, x)*h, x=np.linspace(0, 1, num=num_discretization)
+    c = kernel(np.linspace(0, 1, num=num_discretization)[:, None])[0] * h 
     A = np.empty((num_discretization, num_discretization))
-    for i in range(num_discretization):
+    for i in range(num_discretization): # kernel's discretization
         A[i, i:] = c[: num_discretization - i]
-        A[i, i::-1] = c[: i + 1]
-    A[:, 0] *= 0.5
-    A[:, -1] *= 0.5
+        A[i, i::-1] = c[: i + 1] # shift to the right one position
+    A[:, 0] *= 0.5 # trapezoidal correction
+    A[:, -1] *= 0.5 # trapezoidal correction
 
     if not eigenfunction:
         return np.flipud(np.sort(np.real(np.linalg.eigvals(A))))[:num]
 
-    eigval, eigvec = np.linalg.eig(A)
+    eigval, eigvec = np.linalg.eig(A) # computation of eigenvalues and eigenvectors
     eigval, eigvec = np.real(eigval), np.real(eigvec)
     idx = np.flipud(np.argsort(eigval))[:num]
     eigval, eigvec = eigval[idx], eigvec[:, idx]
     integrals = np.trapz(eigvec ** 2, dx=h, axis=0) ** 0.5
-    eigvec /= integrals.reshape(1, -1)
+    eigvec /= integrals.reshape(1, -1) # normalization of eigenvectors
     indexes = (np.round(np.linspace(0, 1, num=Nx), 2) * 100).astype(int)
     return eigval, eigvec[indexes, :]
